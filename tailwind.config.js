924c66312b95be45ebd64bd48741f59d98337b5f
Evar-SF27 <evarfomuso29@gmail.com>
2023-07-18 00:40:52 +0100

    hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
         'md': '750px',
         // => @media (min-width: 768px) { ... }
   
-        'lg': '1024px',
+        'lg': '1025px',
         // => @media (min-width: 1024px) { ... }
   
         'xl': '1280px',
