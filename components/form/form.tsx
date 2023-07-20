20914262214f733b137e60d2289cb19e4a0cc28e
Evar-SF27 <evarfomuso29@gmail.com>
2023-07-20 23:16:15 +0100

    added refresh token hook

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import { useState, useRef } from 'react'
-import * as yup from 'yup'
+import { useState, useRef, LegacyRef } from 'react'
 import { Formik } from 'formik'
-// import { useRouter } from 'next/router'
+import { axiosPrivate as axios } from '@/config/axios' 
+import { useRouter } from 'next/navigation'
+import * as yup from 'yup'
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/20/solid'
 
 const registerSchema = yup.object().shape({
     username: yup.string().required("Username is required"),
@@ -23,27 +25,81 @@ const initialValuesRegister = {
 }
 
 const initialValuesLogin = {
+    username: "",
     email: "",
     password: ""
 }
 
 const Form = () => {
     const [authType, setAuthType] = useState("register")
-    const [errorMessage, setErrorMessage] = useState("e")
+    const [errorMessage, setErrorMessage] = useState("")
     const [toggledPassword, setToggledPassword] = useState(false)
 
-    // const router = useRouter()
-    const errRef = useRef()
+    const router = useRouter()
+    // const userRef = useRef<HTMLInputElement | undefined>(null)
+    // const legacyUserRef: LegacyRef = userRef
+    const legacyErrorRef = useRef<HTMLDivElement>()
+    // const legacyErrorRef: LegacyRef<HTMLDivElement | null | undefined> = errRef
     const isLogin = authType === "login"
     const isRegister = authType === "register"
 
-    const register = async () => {}
+    const register = async (values: any, onSubmitProps: any) => {
+        try {
+            const response = await axios.post(
+                "api/auth/register",
+                JSON.stringify(values)
+            )
 
-    const login = async () => {}
+            if(response) {
+                router.push("/")
+            }
+        } catch (err: any) {
+            if (!err?.response) {
+                setErrorMessage("No Server Response")
+            } else if (err.response?.status === 409) {
+                setErrorMessage("User with this email already exists")
+            } else {
+                setErrorMessage(err.message)
+            }
 
-    const handleFormSubmit = async () => {
-        if (isLogin) await login()
-        if (isRegister) await register()
+            // legacyErrorRef.current?.focus() 
+        } finally {
+            onSubmitProps.resetForm()
+        }
+    }
+
+    const login = async (values: any, onSubmitProps: any) => {
+        try {
+            const response = await axios.post(
+                "api/auth/login",
+                JSON.stringify(values)
+            )
+
+            if(response) {
+                router.push("/")
+            }
+        } catch (err: any) {
+            if (!err?.response) {
+                setErrorMessage(err.message)
+            } else if (err.response?.status === 400) {
+                setErrorMessage("Incomplete credentials: Username or Password is missing")
+            } else if (err.response?.status === 401) {
+                setErrorMessage("Unauthorised")
+            } else if (err.response?.status === 404) {
+                setErrorMessage("User not found")
+            } else {
+                setErrorMessage("Login failed")
+            }
+
+            // legacyErrorRef.current?.focus() 
+        } finally {
+            onSubmitProps.resetForm()
+        }
+    }
+
+    const handleFormSubmit = async (values: any, onSubmitProps: any) => {
+        if (isLogin) await login(values, onSubmitProps)
+        if (isRegister) await register(values, onSubmitProps)
     }
 
     const handleTogglePassword = () => {
@@ -95,6 +151,13 @@ const Form = () => {
                                                 <input
                                                     placeholder="Username"
                                                     className="input"
+                                                    onBlur={handleBlur}
+                                                    onChange={handleChange}
+                                                    onClick={resetError}
+                                                    value={values.username}
+                                                    // error={Boolean(touched.username) && Boolean(errors.username)}
+                                                    // helperText={touched.username && errors.username}
+                                                    name="username"
                                                 />
                                             </div>
                                         )}
@@ -102,13 +165,31 @@ const Form = () => {
                                             <input 
                                                 placeholder="Email"
                                                 className="input"
+                                                type="email"
+                                                // ref={userRef}
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                onClick={resetError}
+                                                value={values.email}
+                                                name="email"
                                             />
                                         </div>
                                         <div className="input_container">
                                             <input 
                                                 placeholder="Password"
+                                                type={`${toggledPassword ? "text" : "password"}`}
                                                 className="input"
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                onClick={resetError}
+                                                min={8}
+                                                value={values.password}
+                                                name="password"
                                             />
+                                            {toggledPassword ? 
+                                                <EyeIcon className="icons-small text-primary" onClick={handleTogglePassword} /> 
+                                                : 
+                                                <EyeSlashIcon className="icons-small text-primary" onClick={handleTogglePassword} />}
                                         </div>
                                         <div
                                             className={`${isRegister && "hidden"} flex justify-end text-[12px] -mt-2 mr-2 font-semibold text-gray-500`}
@@ -116,6 +197,7 @@ const Form = () => {
                                             <a href="#">Forgot Password?</a>
                                         </div>
                                         <button
+                                            type="submit"
                                             className="mt-8 bg-primary w-[100%] py-4 rounded-[5px] text-white text-[18px] font-semibold"
                                         >
                                             {isLogin ? "SIGN IN" : "SIGN UP"}
