e03e8b0a4d336b7310d17774dfd1d13cd5de1926
Evar-SF27 <evarfomuso29@gmail.com>
2023-09-05 22:14:07 +0100

    poster

--- components/poster/posters.tsx
+++ components/poster/posters.tsx
@@ -1,17 +1,17 @@
-import svg from '@/utils/svgs/ecommerce-web-page-animate.svg'
+import svg from '@/public/svgs/ecommerce-checkout-laptop-animate.svg'
 import Image from 'next/image'
 
 export const PosterOne = () => {
     return (
-        <div className="bg-secondary-100 flex p-16 gap-4">
+        <div className="bg-secondary-100 flex p-16 gap-8">
             <div>
-                <Image src={svg} alt="poster_one" width={500} />
+                <Image src={svg} alt="poster_one" width={450} />
             </div>
-            <div className="px-12">
+            <div className="px-12 flex flex-col justify-center">
                 <h1 className="text-primary text-[70px] font-bold">Market Space</h1>
                 <p className="text-[24px] font-bold -mt-4">Where Desire meets Satisfaction</p>
                 <p className="text-[18px]">Visit countless stores in your location and get your products at your doorsteps.<br /> No Stress, No Hassle</p>
-                <button className="px-8 py-4 bg-primary rounded-xl text-white mt-8">Shop Now</button>
+                <button className="px-8 py-4 bg-primary rounded-xl text-white mt-8 w-[150px]">Shop Now</button>
             </div>
         </div>
     )
--- components/sales/sales.tsx
+++ components/sales/sales.tsx
@@ -1,18 +1,19 @@
+import './styles/index.css'
+
 const Sales = () => {
   return (
     <div className="lg:grid grid-cols-3 gap-2">
-        <div className="h-[450px] bg-secondary-500 max-lg:h-[400px]">
-            One
+        <div className="h-[500px] max-lg:h-[400px] background_one backdrop">
         </div>
         <div className="md:flex lg:flex-col gap-2">
-            <div className="h-[221px] bg-secondary-500 md:w-[50%] lg:w-[100%]">
+            <div className="h-[246px] md:w-[50%] lg:w-[100%] background_two backdrop">
                 Two
             </div>
-            <div className="h-[221px] bg-secondary-500 md:w-[50%] lg:w-[100%]">
+            <div className="h-[246px] md:w-[50%] lg:w-[100%] background_three backdrop">
                 Three
             </div>
         </div>
-        <div className="h-[450px] bg-secondary-500 max-lg:h-[400px]">
+        <div className="h-[500px] max-lg:h-[400px] background_four backdrop">
             One
         </div>
     </div>
